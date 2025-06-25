04505217ebbd2a862f31f2be2e002a863b69d1d9
Peter Schneider-Kamp <petersk@imada.sdu.dk>
2025-06-25 06:47:41 +0200

    giga distill

---
+++ test/giga_gemma3_1b_distill.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+pip install -e .
+# mld-regmix test/mixture/test.txt --num-epochs 10 --val-every 10 --save-every 100 $@
+# mld-regmix test/mixture/test.txt --num-epochs 10 --val-every 10 --save-every 100 --pretrained $@
+# mld-regmix test/mixture/test.txt --num-epochs 10 --val-every 10 --save-every 100 --distillation $@
+# mld-regmix test/mixture/test.txt --num-epochs 10 --val-every 10 --save-every 100 --pretrained --distillation $@
+# mld-standard test/gemma3/train_test1.parquet --val-data-files test/gemma3/train_test2.parquet --num-epochs 10 --val-every 10 --save-every 100 $@
+# mld-standard test/gemma3/train_test1.parquet --val-data-files test/gemma3/train_test2.parquet --num-epochs 10 --val-every 10 --save-every 100 --pretrained $@
+# mld-standard test/gemma3/train_test1.parquet --val-data-files test/gemma3/train_test2.parquet --num-epochs 10 --val-every 10 --save-every 100 --distillation $@
+#-m mldistill.standard test/gemma3/train_test1.parquet --val-data-files test/gemma3/train_test2.parquet --num-epochs 10 --val-every 10 --save-every 100 --pretrained --distillation $@
+export NCCL_MAX_NCHANNELS=72
+export NCCL_MIN_NCHANNELS=72
+export JOBID=giga_gemma3_1b_distill
+mkdir -p logs/$JOBID
+accelerate launch \
+  --multi_gpu \
+  --gpu_ids all \
+  --num_processes 2 \
+  --num_machines 1 \
+  --main_process_port 29501 \
+  --machine_rank 0 \
+  --mixed_precision bf16 \
+  -m mldistill.standard ../../data/train-giga-gemma3\
+  --val-data-files ../../data/valid-giga-gemma3 \
+  --max-seq-length 6144 \
+  --distillation \
+  --batch-size 1 \
+  --gradient-accumulation 32 \
+  --student models/gemma-3-1b-pt \
+  --run-id $JOBID \
+  --pretrained \
+  --teacher models/gemma-3-4b-pt \
+  --learning-rate 1e-5 \
+  --val-every 100 \
+  --save-every 100 \
+> >(tee logs/$JOBID/stdout.txt) \
+2> >(tee logs/$JOBID/stderr.txt >&2)
+
+#  --collect-every 1 \
+#  --offload-optimizer \
