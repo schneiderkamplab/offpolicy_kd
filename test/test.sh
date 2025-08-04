f77f8a6efda08cb00a27391ddfa450d39f9e6498
AndreaBlasi97 <andreablasi1997@gmail.com>
2025-08-04 13:23:37 +0200

    changed test

--- test/test.sh
+++ test/test.sh
@@ -1,40 +1,13 @@
 #!/bin/bash
-pip install -e .
-# mld-regmix test/mixture/test.txt --num-epochs 10 --val-every 10 --save-every 100 $@
-# mld-regmix test/mixture/test.txt --num-epochs 10 --val-every 10 --save-every 100 --pretrained $@
-# mld-regmix test/mixture/test.txt --num-epochs 10 --val-every 10 --save-every 100 --distillation $@
-# mld-regmix test/mixture/test.txt --num-epochs 10 --val-every 10 --save-every 100 --pretrained --distillation $@
-# mld-standard test/gemma3/train_test1.parquet --val-data-files test/gemma3/train_test2.parquet --num-epochs 10 --val-every 10 --save-every 100 $@
-# mld-standard test/gemma3/train_test1.parquet --val-data-files test/gemma3/train_test2.parquet --num-epochs 10 --val-every 10 --save-every 100 --pretrained $@
-# mld-standard test/gemma3/train_test1.parquet --val-data-files test/gemma3/train_test2.parquet --num-epochs 10 --val-every 10 --save-every 100 --distillation $@
-#-m mldistill.standard test/gemma3/train_test1.parquet --val-data-files test/gemma3/train_test2.parquet --num-epochs 10 --val-every 10 --save-every 100 --pretrained --distillation $@
-export NCCL_MAX_NCHANNELS=72
-export NCCL_MIN_NCHANNELS=72
-export JOBID=test
-mkdir -p logs/$JOBID
-accelerate launch \
-  --multi_gpu \
-  --gpu_ids all \
-  --num_processes 2 \
-  --num_machines 1 \
-  --machine_rank 0 \
-  --mixed_precision bf16 \
-  -m mldistill.standard ../../data/train-giga-gemma3\
-  --val-data-files ../../data/valid-giga-gemma3 \
-  --max-seq-length 2048 \
-  --batch-size 1 \
-  --gradient-accumulation 1 \
-  --student models/gemma-3-1b-pt \
-  --run-id $JOBID \
-  --pretrained \
-  --teacher models/gemma-3-4b-pt \
-  --learning-rate 1e-5 \
-  --val-every 100 \
-  --save-every 100 \
-  --collect-every 1 \
-  --attn-implementation eager \
-  --overwrite \
-  --yes \
-  --compile \
-> >(tee logs/$JOBID/stdout.txt) \
-2> >(tee logs/$JOBID/stderr.txt >&2)
+#pip install .
+echo "Running tests with mld-regmix and mld-standard"
+
+mld-regmix test/mixture/test.txt  --num-epochs 10 --val-every 10 --save-every 10 --pretrained --distillation --on_policy
+#mld-regmix test/mixture/test.txt --num-epochs 10 --val-every 10 --save-every 10
+#mld-regmix test/mixture/test.txt --num-epochs 10 --val-every 10 --save-every 10 --pretrained
+#mld-regmix test/mixture/test.txt --num-epochs 10 --val-every 10 --save-every 10 --distillation
+#mld-regmix test/mixture/test.txt --num-epochs 10 --val-every 10 --save-every 10 --pretrained --distillation
+#mld-standard test/gemma3/train_test1.parquet --val-data-files test/gemma3/train_test2.parquet --num-epochs 10 --val-every 10 --save-every 10
+#mld-standard test/gemma3/train_test1.parquet --val-data-files test/gemma3/train_test2.parquet --num-epochs 10 --val-every 10 --save-every 10 --pretrained
+#mld-standard test/gemma3/train_test1.parquet --val-data-files test/gemma3/train_test2.parquet --num-epochs 10 --val-every 10 --save-every 10 --distillation
+#mld-standard test/gemma3/train_test1.parquet --val-data-files test/gemma3/train_test2.parquet --num-epochs 10 --val-every 10 --save-every 10 --pretrained --distillation
